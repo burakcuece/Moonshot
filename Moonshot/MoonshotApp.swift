fbc3c4800cd575f8eccff3553c597e9fca9fd9c3
Burak Cüce <b.cuece@web.de>
2022-08-02 12:06:33 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Burak Cüce on 02.08.22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

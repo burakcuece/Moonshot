09f030bf513870a586f3fa61f99298116d7ab063
Burak Cüce <b.cuece@web.de>
2022-08-03 10:46:14 +0200

    - add Images to Assets, Astronaut, Bundle-Decodable, missions, json and astronauts.json - edit ContentView

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Burak Cüce on 03.08.22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
